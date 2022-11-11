b1177f4839376a16e1316711e89a155ac4613e4d
RSLilova18 <RSLilova18@codingburgas.bg>
2022-11-11 10:16:25 +0200

    classwork 10.11 update

--- src/main/java/com/example/baza/BazaApplication.java
+++ src/main/java/com/example/baza/BazaApplication.java
@@ -1,19 +1,44 @@
 package com.example.baza;
 
-import java.sql.Date;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BazaApplication {
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
 
+@SpringBootApplication
+public class BazaApplication implements CommandLineRunner {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    PlayerDao dao;
     public static void main(String[] args) {
         SpringApplication.run(BazaApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info("Inserting Player 4: {}",dao.insertPlayer( new Player(4, "Thiem", "Austria", new Date(System.currentTimeMillis()),17)));
 
-    dao.createTournamentTable();
+        //String Date1 ="31/12/1998";
 
-       logger.info("French Players: {}", dao.getPlayerByNationality("France") );
+        //logger.info("Updating Player with Id 4: {}", dao.updatePlayer(
+        //  new Player(4, "Thiem" , "Austria" , new SimpleDateFormat("dd/MM/yyyy").parse(Date1),17)));
+
+        //logger.info("Deleting Player with Id: 2 {}",dao.deletePlayerById(1));
+
+        dao.createTournamentTable();
+
+        logger.info("French Players: {}", dao.getPlayerByNationality("France") );
+
+        logger.info("All Players Data: {}",dao.getAllPlayers());
+    }
 }
